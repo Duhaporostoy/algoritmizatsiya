798640f040158f4647ec429d970f883e74188c11
Duhaporostoy <165923283+Duhaporostoy@users.noreply.github.com>
2026-02-28 21:54:22 +0300

    organize projects into Lab1 Lab2 Lab3 folders

--- ДЗ Лабораторная работа №3 Алгоритмы сортировки/SortingProject/test_utils.h
+++
@@ -1,9 +0,0 @@
-// test_utils.h
-#pragma once
-#include <vector>
-
-// Проверяет, отсортирован ли массив по возрастанию
-bool isSorted(const std::vector<int>& arr);
-
-// Сравнивает два массива на равенство
-bool areEqual(const std::vector<int>& a, const std::vector<int>& b);
